9a6e09db1555983095c206e970883ffa39476d1a
Adnan RIHAN <axel50397@gmail.com>
2018-06-23 08:11:58 +0100

    Carbon 1.30.0 (#1942)
    Improved dashing script

--- docsets/Carbon/generate.bash
+++ docsets/Carbon/generate.bash
@@ -1,33 +1,63 @@
 #!/bin/bash
 
-DASHING=
-WGET=wget
+REPO="https://github.com/briannesbitt/Carbon"
+#HTML="https://raw.githubusercontent.com/briannesbitt/Carbon/gh-pages/docs/index.html"
+HTML="http://carbon.nesbot.com/docs/"
+VERSION_TAG="$1"
 
-# Check if wget is available
-if [[ -z `which wget` ]]; then
-    (>&2 echo "Sorry but wget isn't available in your PATH. I need it.")
+DASHING="`which dashing`"
+DOXYGEN="`which doxygen`"
+GIT="`which git`"
+WGET="`which wget`"
+TEMPDIR="`mktemp -d`"
+
+# Check input
+if [[ -z $VERSION_TAG ]] || [[ $VERSION_TAG == '-h' ]] || [[ $VERSION_TAG == '--help' ]]; then
+    echo "Usage: $0 <tag version>" >&2
+    echo "Ex: $0 1.29.5" >&2
     exit 1
 fi
 
-# Check if dashing is available in $PATH, download it otherwise
-if [[ -n `which dashing` ]]; then
-    DASHING=`which dashing`
-else
-    echo -n "Dashing isn't available on your system. Downloading it... "
-    wget --no-check-certificate -q https://github.com/technosophos/dashing/releases/download/0.3.0/dashing
-    DASHING=./dashing
-    echo "Done !"
+# Check if dashing is available in $PATH
+if [[ -z $DASHING ]]; then
+    echo "Sorry but dashing isn't available in your PATH. I need it." >&2
+    exit 1
 fi
 
-# Download HTML docs
+# Check if doxygen is available in $PATH
+if [[ -z $DOXYGEN ]]; then
+    echo "Sorry but doxygen isn't available in your PATH. I need it." >&2
+    exit 1
+fi
+
+# Check if git is available in $PATH
+if [[ -z $GIT ]]; then
+    echo "Sorry but git isn't available in your PATH. I need it." >&2
+    exit 1
+fi
+
+# Check if wget is available
+if [[ -z $WGET ]]; then
+    echo "Sorry but wget isn't available in your PATH. I need it." >&2
+    exit 1
+fi
+
+# Download repo archive at given tag, and unzip it
 echo -n "Downloading HTML docs... "
-wget -kpHN -nH -P dashing --no-check-certificate -q http://carbon.nesbot.com/docs/
+$WGET -q -kpHN -nH -P docs "$HTML"
+# echo "Downloading repo at: $TEMPDIR"
+# $WGET -q -O "$TEMPDIR/$(basename $REPO)-$VERSION_TAG.zip" "$REPO/archive/$VERSION_TAG.zip"
 echo "Done !"
 
 # Generate .docset and archive it
 echo -n "Building .docset ... "
-$DASHING build -s dashing > /dev/null
+$DASHING build -s docs/ > /dev/null
+tar --exclude='.DS_Store' -cvzf "$(basename $REPO)-$VERSION_TAG.tgz" Carbon.docset
 echo "Done !"
+
+# Clean directory
+rm -rf Carbon.docset/
+rm -rf docs/
 
 echo
 echo "Docset successfully generated !"
