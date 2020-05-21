fe1bdfbb43d9f0de7c27ca1e844fd1d325b8020e
Clayton Carter <claytonrcarter@gmail.com>
2020-05-21 13:03:39 -0400

    Update Carbon PHP generation script to include API docs and more types in generated docset

--- docsets/Carbon/sami-config.php
+++ docsets/Carbon/sami-config.php
@@ -2,21 +2,28 @@
 
 ini_set('memory_limit', -1);
 
+// disable deprecation warnings in Sami
+// (though these will need to be fixed eventually!)
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 use Sami\Sami;
 use Sami\Version\GitVersionCollection;
 use Sami\Version\SingleVersionCollection;
 use Symfony\Component\Finder\Finder;
 
+$src = __DIR__ . '/Carbon/src';
+$version = getenv('VERSION_TAG');
+
 $iterator = Finder::create()
                   ->files()
                   ->name('*.php')
-                  ->in($src = __DIR__ . '/carbon/src');
+                  ->in($src);
 
 $versions = GitVersionCollection::create($src)
-                                ->addFromTags('2.34.2');
+                                ->addFromTags($version);
 
 return new Sami($iterator, [
-    'build_dir' => __DIR__ . '/docset',
+    'build_dir' => __DIR__ . '/docs/api_docs',
     'cache_dir' => "$src/../cache",
     'title' => 'Carbon',
     'versions' => $versions,
