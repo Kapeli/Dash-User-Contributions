268221dd78d7f43f99d227b6dc93fad8114db8d4
benbenbang <ben.chen@vestiairecollective.com>
2020-06-29 20:53:02 +0200

    Update README

--- docsets/aiomultiprocess/build.sh
+++ docsets/aiomultiprocess/build.sh
@@ -1,7 +1,7 @@
 # @Author: benbenbang
 # @Date:   2020-06-21
 # @Last Modified by:   benbenbang
-# @Last Modified time: 2020-06-21
+# @Last Modified time: 2020-06-29
 
 mkdir target
 git clone https://github.com/omnilib/aiomultiprocess.git ./target
@@ -15,5 +15,4 @@ mkdir -p versions/$AIOMULTIPROCESS_VERSION/
 tar --exclude='.DS_Store' -czvf aiomultiprocess.tgz aiomultiprocess.docset
 cp aiomultiprocess.tgz versions/$AIOMULTIPROCESS_VERSION/
 rm -rf aiomultiprocess.docset
-mv aiomultiprocess.tgz aiomultiprocess.tgz.txt
-mv versions/$AIOMULTIPROCESS_VERSION/aiomultiprocess.tgz versions/$AIOMULTIPROCESS_VERSION/aiomultiprocess.tgz.txt
+mv versions/$AIOMULTIPROCESS_VERSION/aiomultiprocess.tgz versions/$AIOMULTIPROCESS_VERSION/aiomultiprocess.tgz
