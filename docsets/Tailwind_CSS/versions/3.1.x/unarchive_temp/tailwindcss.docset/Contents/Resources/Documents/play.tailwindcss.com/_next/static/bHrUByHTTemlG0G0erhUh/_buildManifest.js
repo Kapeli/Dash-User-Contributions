fdb3122b4f556dd418079c922aad6c7091d489cf
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-18 01:07:09 +0800

    add Tailwind CSS 3.1.3

---
+++ docsets/Tailwind_CSS/versions/3.1.x/unarchive_temp/tailwindcss.docset/Contents/Resources/Documents/play.tailwindcss.com/_next/static/bHrUByHTTemlG0G0erhUh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-25839e52160ad85d.js"],"/[[...slug]]":["static/chunks/1653-d5f1d15f1db2e959.js","static/chunks/pages/[[...slug]]-4b8c88f85eb565c5.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
