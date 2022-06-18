ad92405603e470cfaa145e4399fed14612339e18
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-18 21:53:39 +0800

    remove unarchive dir

--- docsets/Tailwind_CSS/versions/3.1.x/unarchive_temp/tailwindcss.docset/Contents/Resources/Documents/tailwindcss.com/_next/static/chunks/pages/examples/mobile-first-demo-4cbfedc32e1438e56667.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2510],{2684:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/examples/mobile-first-demo",function(){return e(31112)}])}},function(n){n.O(0,[1112,9774,2888,179],(function(){return _=2684,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
