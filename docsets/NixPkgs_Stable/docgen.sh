b5e657ef29e076a8a514a05f17e3a22fd64cf2e4
aiotter <git@aiotter.com>
2022-09-29 16:27:42 +0900

    Remove unnecesary resources from NixPkgs_Stable docset

--- docsets/NixPkgs_Stable/docgen.sh
+++ docsets/NixPkgs_Stable/docgen.sh
@@ -8,6 +8,6 @@ sed -Ei '' \
   -e 's/<section class="generic-layout docbook-page">(.*<section class="chapter">)/\1/' \
   nixos.org/manual/nixpkgs/stable/index.html
 
-dashing build
+dashing build --source=nixos.org
 
 tar -czvf NixPkgs_Stable.tgz nixpkgs_stable.docset
